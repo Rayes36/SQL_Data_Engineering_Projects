205678b91889ef0380369723d1c5de75f9455119
Rayes36 <rayesjordan@gmail.com>
2026-04-27 18:01:35 +0700

    Added table validation

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -81,3 +81,17 @@ INSERT INTO dw_marts.main.skills_job_dim(
     FROM
         read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', AUTO_DETECT=TRUE)
 ;
+
+SELECT '=== Validation ===' AS info;
+SELECT 'company_dim' AS name, format('{:,}', COUNT(*)) AS total_rows FROM dw_marts.main.company_dim
+UNION ALL
+SELECT 'skills_dim', format('{:,}', COUNT(*)) FROM dw_marts.main.skills_dim
+UNION ALL
+SELECT 'job_postings_fact', format('{:,}', COUNT(*)) FROM dw_marts.main.job_postings_fact
+UNION ALL
+SELECT 'skills_job_dim', format('{:,}', COUNT(*)) FROM dw_marts.main.skills_job_dim;
+
+SELECT * FROM dw_marts.main.company_dim LIMIT 5;
+SELECT * FROM dw_marts.main.skills_dim LIMIT 5;
+SELECT * FROM dw_marts.main.job_postings_fact LIMIT 5;
+SELECT * FROM dw_marts.main.skills_job_dim LIMIT 5;
